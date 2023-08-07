fcebb71e96de65d0dfce32e5a524c3990c1826a3
kri-sh27 <hogalekrishna2000@gmail.com>
2023-08-07 15:03:43 +0530

    started

---
+++ tic-taac-toe/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Card from './components/Card/Card'
+
+function App() {
+
+  return (
+    <>
+      {/* hello */}
+      {/* <Icon name="cross"/> */}
+      <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ tic-taac-toe/src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import Icon from "../Icon/Icon";
+import  "./Card.css"
+function Card(){
+
+    return(
+        <div className="card">
+    <Icon/>
+        </div>
+    )
+}
+export default Card;
